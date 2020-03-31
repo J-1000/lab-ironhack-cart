f064017128139acf77465c1eab11664b583910e2
Chantel Taki <chanteltaki@gmail.com>
2020-03-31 08:58:20 -0400

    On iteration 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,31 @@ var $calc = document.getElementById('calc');
 
 function updateSubtotal($product) {
   // Iteration 1.1
+  let subtot = document.querySelector('.subtot span').innerText;
+  let productPrice = document.querySelector(".pu span").innerText;
+  //console.log(productPrice);
+  let totalQty = document.querySelector('.qty input').value;
+ // console.log(totalQty);
+  let subtotal = productPrice * totalQty;
+  return subtot= subtotal;
+  //console.log(subtot);
+ 
 }
 
 function calculateAll() {
   // Iteration 1.2
-}
+  //let prices = document.querySelector(".subtot").innerText;
+  let totalPrice= 0;
+  for (product in cart){
+    //updateSubtotal(product);
+    totalPrice += updateSubtotal(product);
+    }
+  document.querySelector("h2").innerHTML = `<h2>Total: <span>${totalPrice}</span></h2>`;
+
+  }
+
+  
 $calc.onclick = calculateAll;
+
+updateSubtotal();
+calculateAll();
