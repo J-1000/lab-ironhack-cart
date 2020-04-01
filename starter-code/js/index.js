c06417eb5372ece15deef08e1337691bd56bdf89
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-01 23:41:34 +0200

    I got the 2 static rows to delete but not the one I created.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,41 +1,54 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
-var $addP = document.getElementById('create');
+const $cart = document.querySelector('#cart tbody');
+const $calc = document.getElementById('calc');
+const $addP = document.getElementById('create');
 
 // Just a thought we had -\o/- decided not to go with it
-let products = [
-  { name: 'IronBubble-head', unitPrice: 25},
-  { name: 'Markus', unitPrice: 5},
-] 
-  
+// let products = [{
+//     name: 'IronBubble-head',
+//     unitPrice: 25
+//   },
+//   {
+//     name: 'Markus',
+//     unitPrice: 5
+//   },
+// ]
+
 
 function updateSubtotal($product) {
-  let totalPrice = 0;
-    for(let i = 0; i < $cart.rows.length; i++){
-        let rowQuantity = parseInt($cart.rows[i].querySelector('.qty label input').value);
-        let rowPrice = parseInt($cart.rows[i].querySelector('.pu span').innerText);
-        let subtotal = rowPrice*rowQuantity;
-        $cart.rows[i].querySelector('.subtot span').innerText = subtotal;
-    }
+  for (let i = 0; i < $cart.rows.length; i++) {
+    let rowQuantity = parseInt($cart.rows[i].querySelector('.qty label input').value);
+    let rowPrice = parseInt($cart.rows[i].querySelector('.pu span').innerText);
+    let subtotal = rowPrice * rowQuantity;
+    $cart.rows[i].querySelector('.subtot span').innerText = subtotal;
   }
-  
+}
+
 
 function calculateAll() {
-   // Iteration 1.2
-   let sumPrice = 0;
-   updateSubtotal();
-   for(let i = 0; i < $cart.rows.length; i++){
+  // Iteration 1.2
+  let sumPrice = 0;
+  updateSubtotal();
+  for (let i = 0; i < $cart.rows.length; i++) {
     let rowSubTotal = parseInt($cart.rows[i].querySelector('.subtot span').innerText)
     sumPrice += rowSubTotal
   }
   document.querySelector('h2 span').innerText = sumPrice;
+};
+
+document.querySelectorAll(".btn-delete").forEach(function (button) {
+  button.onclick = deleteButton;
+});
+
+function deleteButton(element) {
+  const deleteProduct = element.target.parentElement.parentElement;
+  $cart.removeChild(deleteProduct);
 }
 
-function addProduct(){
-  let newRow = document.querySelector('#cart tbody');
-  let addNewRow = newRow.insertRow(-1);
+function addProduct() {
+  let addNewRow = $cart.insertRow(-1);
   let setPrice = document.querySelector('.pUnit').value;
   let setName = document.querySelector('.pName').value;
+  addNewRow.className = 'product';
   addNewRow.innerHTML = `
   <tr class="product">
   <td class="name">
@@ -56,12 +69,6 @@ function addProduct(){
   <button class="btn btn-delete">Delete</button>
   </td>
   </tr>` // I know its quite a hardcode but I couldnt come up with anything else,
-}        // wasted a couple hours trying to do it :((
-
-function deleteProduct(){
-  //$cart.rows.remove();
-}
+}; // wasted a couple hours trying to do it :((
 $calc.onclick = calculateAll;
 $addP.onclick = addProduct;
-
-
