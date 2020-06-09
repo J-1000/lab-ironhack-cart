3c4dd07a3963c1535355834474ab1b276595076e
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-06-09 13:48:24 +0200

    Iteration 1/2/3, done in just one function

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,27 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtotal($product) {
-  // Iteration 1.1
+let btn = document.querySelector(".btn-success")
+btn.addEventListener("click", updateAll)
+
+function updateAll($product) {
+  // Iteration 1 to 3
+  let products = document.querySelectorAll(".product")
+  let total = 0;
+  for (const product of products) {
+    let price = product.querySelector(".pu>span").innerText;
+    let multiply = product.querySelector(".qty input").value;
+    let subTotal = price * multiply
+    product.querySelector(".subtot span").innerText = subTotal;
+    total += subTotal;
+    document.querySelector("h2 span").innerText = total;
+  }
 }
 
-function calculateAll() {
-  // Iteration 1.2
-}
-$calc.onclick = calculateAll;
+// console.log(updateSubtotal());
+
+// function calculateAll() {
+//   // Iteration 1.2
+// }
+// $calc.onclick = calculateAll;
+
