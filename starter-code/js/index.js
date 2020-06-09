e361d165fc51e0297df8c67f7826df32418dac22
carlosziegler <carlos.ziegler@gmail.com>
2020-06-09 13:28:34 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,6 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-const cartState = {
-  total: 0,
-}
 function updateSubtotal($product) {
 
   const qtd = $product.querySelector('.qty input').value
@@ -24,18 +21,85 @@ function calculateAll() {
   }, 0)
 
   return document.querySelector('body > h2').innerHTML = `Total: $ ${total}`
+}
+
+// remove product of cart
+function removeProduct(product) {
+  $cart.removeChild(product)
+  $cart = document.querySelector('#cart tbody');
+}
+
+$calc.onclick = calculateAll;
+
+// addEventListener each product
+
+function addEventListenerEachProduct() {
+  const products = $cart.querySelectorAll("#cart > tbody> .product")
+
+  products.forEach((item) => {
+    item.querySelector('.qty input').addEventListener("change", function (event) {
+      updateSubtotal(item)
+    })
+    console.log(item)
+    item.querySelector('.rm > button').addEventListener("click", function (event) {
+      removeProduct(item)
+    })
+
+  })
+}
+addEventListenerEachProduct()
+
+function createProduct(name, price) {
+  const element = document.createElement('tr')
+  element.classList.add('product')
+  element.innerHTML = `
+      <td class="name">
+        <span>${name}</span>
+      </td >
+
+          <td class="pu">
+            $<span>${price}.00</span>
+          </td>
+
+          <td class="qty">
+            <label>
+              <input type="number" value="0" min="0">
+        </label>
+      </td>
+
+            <td class="subtot">
+              $<span>0</span>
+            </td>
+
+            <td class="rm">
+              <button class="btn btn-delete">Delete</button>
+            </td>
+`
+  $cart.appendChild(element)
 
 
 }
-$calc.onclick = calculateAll;
 
-const products = $cart.querySelectorAll("#cart > tbody> .product")
-products.forEach((item) => {
-  item.querySelector('.qty input').addEventListener("change", function (event) {
-    updateSubtotal(item)
+function addEventListenerButtonCreateProduct() {
+  const btnAddProduct = document.querySelector('#create')
+  const productName = document.querySelector('#cart > tfoot > tr > td:nth-child(1) > input[type=text]')
+  const unitPrice = document.querySelector('#cart > tfoot > tr > td:nth-child(2) > input[type=number]')
+  btnAddProduct.addEventListener('click', function () {
+    console.log('click')
+    console.log(productName.value)
+    console.log(unitPrice.value)
+
+    createProduct(productName.value, unitPrice.value)
+    productName.value = ''
+    unitPrice.value = ''
+    $cart = document.querySelector('#cart tbody');
+    addEventListenerEachProduct()
   })
 
-})
+
+
+}
+addEventListenerButtonCreateProduct()
 
 
 
