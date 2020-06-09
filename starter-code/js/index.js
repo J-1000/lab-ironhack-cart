c8a96402bf2ec937a0d7e43a926ace43e9702a31
Marco Muser <marco.muser@posteo.de>
2020-06-09 14:38:54 +0200

    add iterations 1-3 and beginning of 4

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,34 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $del = document.querySelectorAll('.btn.btn-delete');
 
-function updateSubtotal($product) {
-  // Iteration 1.1
+
+let products = document.querySelectorAll('.product');
+function updateSubtotal() {
+//   // Iteration 1.1
+  products.forEach(element => {
+    let qty = (element.querySelector("input").value)
+    let perUnit = +element.querySelector(".pu span").innerText;
+    let subtotal = qty * perUnit;
+    element.querySelector('.subtot span').innerText = subtotal;
+  });
 }
+
 
 function calculateAll() {
   // Iteration 1.2
+  updateSubtotal()
+  let total = 0;
+  products.forEach(element => {
+    total += +element.querySelector('.subtot span').innerText;
+  });
+  document.querySelector('h2 span').innerText = total;
 }
+
 $calc.onclick = calculateAll;
+
+
+for (let i = 0; i < $del.length; i++) {
+  $del[i].onclick = products[i].removeChild()
+
+}
