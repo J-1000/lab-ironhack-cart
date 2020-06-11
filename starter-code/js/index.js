c9f154477ea73bddc50cd3c8d793fd1834060acc
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-06-11 15:09:33 +0200

    cloned repo

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,35 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtotal($product) {
-  // Iteration 1.1
+
+
+function updateSubtotal(product) {
+  let price  = product.querySelector('.pu span').innerText;
+  let quantity = product.querySelector('.qty label input').value; 
+  let total = price * quantity
+  product.querySelector('.subtot span').innerText = total;
+  return total;
 }
 
+
+
 function calculateAll() {
-  // Iteration 1.2
+  let products = document.querySelectorAll('.product'); 
+  //console.log(products);
+  let total = 0; 
+  products.forEach(function(item, index) {
+    total = total + updateSubtotal(item);
+    console.log(item);
+  })
+  // for(let i = 0; i < products.length; i++) {  example with for each. 
+  //   console.log(products[i], `item ${i}`); 
+  //   total = total + updateSubtotal(products[i]);
+  // }
+  document.querySelector('h2 span').innerHTML = total; 
+  console.log(total, " TOTAL");
 }
+// document.querySelectorAll('.btn .btn-success').addEventListener('click',updateSubtotal)
+
 $calc.onclick = calculateAll;
+
+
